d7581fe5af8a0319d35e92c27d76b718277ecc69
PavanReddy77 <Pavankumarkj347@Gmail.com>
2022-06-23 20:41:16 +0530

    GitMergeRebase

---
+++ GitMerge.java
@@ -0,0 +1,3 @@
+Hi, Welcome to Java World..!!
+Java is a High Level Object Oriented Programming Language..!!
+Java and Selenium WebDriver is the best Combination for Test Automation..!!
---
+++ GitRebase.java
@@ -0,0 +1,3 @@
+Hi, Welcome to Selenium WebDriver API World..!!
+Selenium WebDriver is an API to Automate Web Applications..!!
+Selenium Grid with Docker..!!
